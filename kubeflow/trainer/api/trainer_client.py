afd49d0eb60ec42d4249b36f8a593eb489c243e1
andres75125 <starlord52mcu@gmail.com>
2026-03-18 07:40:08 -0500

    chore(trainer): fix typos in TrainerClient docstrings (#394)
    Fix grammatical error in list_jobs returns description ("TrainJob exist" -> "TrainJobs exist") and add missing period in get_job docstring.
    
    Signed-off-by: andres75125 <starlord52mcu@gmail.com>

--- kubeflow/trainer/api/trainer_client.py
+++ kubeflow/trainer/api/trainer_client.py
@@ -156,7 +156,7 @@ class TrainerClient:
             runtime: Reference to one of the existing runtimes.
 
         Returns:
-            List of created TrainJobs. If no TrainJob exist, an empty list is returned.
+            List of created TrainJobs. If no TrainJobs exist, an empty list is returned.
 
         Raises:
             TimeoutError: Timeout to list TrainJobs.
@@ -165,7 +165,7 @@ class TrainerClient:
         return self.backend.list_jobs(runtime=runtime)
 
     def get_job(self, name: str) -> types.TrainJob:
-        """Get the TrainJob object
+        """Get the TrainJob object.
 
         Args:
             name: Name of the TrainJob.
